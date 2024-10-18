78fc53d91d9c044a4e2d6637c5b41f2713cda9dc
jh080724 <tsj010813@gmail.com>
2024-10-18 11:28:44 +0900

    JPA - 양방향 연관관계 실습 - 데이터 갱신이 반대편에 반영되지 않은 문제 해결

--- src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04/repository/DepartmentRepositoryTest.java
@@ -2,6 +2,7 @@ package com.study.jpa.chap04.repository;
 
 import com.study.jpa.chap04.entity.Department;
 import com.study.jpa.chap04.entity.Employee;
+import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +22,9 @@ class DepartmentRepositoryTest {
 
     @Autowired
     DepartmentRepository departmentRepository;
+
+    @Autowired
+    EntityManager em;
 
     @Test
     @DisplayName("부서 정보 조회하면 해당 부서원들도 함께 조회된다.")
@@ -53,6 +57,38 @@ class DepartmentRepositoryTest {
         System.out.println("\n\n\n");
         System.out.println("employee = " + employee);
         System.out.println("\n\n\n");
+
+    }
+
+    @Test
+    @DisplayName("양방향 연관관계에서 연관 데이터의 수정")
+    void testChangeDept() {
+        // 1사원의 부서를 1 -> 2번 부서로 변경해야 한다.
+
+        // given
+        Employee foundEmp = employeeRepository.findById(1L).orElseThrow();
+
+        Department newDept = departmentRepository.findById(2L).orElseThrow();
+
+//        foundEmp.setDepartment(newDept);
+//
+        // 연관관계 편의 메서드 호출 -> 데이터 수정시에는 반대편 엔터티도 꼭 수정을 해야한다.
+        foundEmp.changeDepartment(newDept);
+
+//        employeeRepository.save(foundEmp);
+//        newDept.getEmployees().add(foundEmp);
+
+//        em.flush(); // DB로 밀어내기
+//        em.clear(); // 영속성 컨텍스트 비우기(비우지 않음, 컨텍스트 내의 정보를 참조하려 하니까)
+
+        // when
+//        Department foundDept = departmentRepository.findById(2L).orElseThrow();
+
+        // then
+        System.out.println("---------------------------------------\n\n\n");
+//        foundDept.getEmployees().forEach(emp-> System.out.println(emp));
+        newDept.getEmployees().forEach(emp-> System.out.println(emp));
+        System.out.println("---------------------------------------\n\n\n");
 
     }
 }
