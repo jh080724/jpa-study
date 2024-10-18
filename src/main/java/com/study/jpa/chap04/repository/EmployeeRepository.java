99c345e03b70433ab672d96ba246a29bdd87ed6a
jh080724 <tsj010813@gmail.com>
2024-10-18 11:50:14 +0900

    JPA - 양방향 연관관계 실습 - N+1문제, FETCH JOIN으로 해결

--- src/main/java/com/study/jpa/chap04/repository/EmployeeRepository.java
+++ src/main/java/com/study/jpa/chap04/repository/EmployeeRepository.java
@@ -1,8 +1,14 @@
 package com.study.jpa.chap04.repository;
 
+import com.study.jpa.chap04.entity.Department;
 import com.study.jpa.chap04.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.List;
+
+public interface EmployeeRepository
+        extends JpaRepository<Employee, Long> {
+
 
 }
